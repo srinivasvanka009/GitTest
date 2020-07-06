f97cb8a5da4aa67c1596ec2e83f4e9dde5d8e7ea
srinivas <srinivas>
2020-07-06 22:49:01 +0530

    Added new files

---
+++ SampleGitTest/NewDirectory/Nothing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleGitTest.NewDirectory
+{
+    public class Nothing
+    {
+        public int MyProperty { get; set; }
+    }
+}
